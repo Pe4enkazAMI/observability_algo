9547b487de583ceefae93eb7af6a7532edd2e7c4
Pe4enkazAMI <80426716+Pe4enkazAMI@users.noreply.github.com>
2022-03-10 01:20:52 +0300

    UPD: Several observation variables || Rank evaluation using SVD

--- Non-Linear.jl
+++ Non-Linear.jl
@@ -3,40 +3,94 @@ using LinearAlgebra
 using Nemo
 
 @variables t x1(t) x2(t) a(t) c(t) d(t)
-D = Differential(t)
-function my_dot_product(X::Any, Y::Any) ## why doesnt built_in work???
-    if (size(X) != size(Y))
-        return 0;
-    end
-    ans = 0
-    for i in 1:size(Y)[1]
-        ans += X[i] * Y[i]
-    end
-    return ans
-end
+
 # Example 1
 DX = [a*x1 + x1*x2, c*x2 + d*x1*x2, 0, 0, 0] ##last 3 eq's are for params "a, c, d"
-y = x1 
-#println(Symbolics.gradient(a*exp(x2), [x1, x2, a], simplify = true))
-#= Example 2
-DX(t) = [x1(t)^2 + 2x1(t)*x2(t), x2(t)^2]
-y(t) = x1(t) + x2(t) 
-=#
+y = x1
 
-function is_NL_Observable(sys::Any, viewable::Any, D::Differential, params::Any)
 
-    Y = [viewable]
 
-    for i in 1:size(sys)[1]-1
+######################## Function for computing Jacobian rank
+function get_rank(Matrix::Any)
+     U, S, VT = svd(Matrix)
+     count = 0
+     for i in S
+          if i == 0
+               break
+          end
+          count += 1
+     end
+     return count
+end 
+########################
 
-        Y = vcat(Y, [my_dot_product(Symbolics.gradient(Y[i], params, simplify = true), sys)])
 
-    end
+######################## Function for computing ans for exact parameter
+function get_ans_exact(Matrix::Any, exact::Any, vars::Any)
+     _Only_Here_ = copy(Matrix)
+
+     before_add = eval(build_function(_Only_Here_, vars)[1])
+
+     rkMat = get_rank(before_add(rand(Int, (1, length(vars)))))
+
+     push!(_Only_Here_, exact)
+
+     after_add = eval(build_function(_Only_Here_, vars)[1])
+
+     rkExMat = get_rank(after_add(rand(Int, (1, length(vars)))))
+     if rkExMat == rkMat
+          return 1
+     else
+          return 0
+     end
+end
+########################
+
+
+
+
+#=function test(sys::Any, viewable::Any, params::Any, exact::Any = nothing)
+     n = length(viewable)
+     for i in (n+1):(length(sys)-1 + n)
+         push!(viewable, dot(Symbolics.gradient(viewable[i - n], params, simplify = true), sys))
+     end
+ 
+     ans = Symbolics.jacobian(Y, params, simplify=true)
+     if exact != nothing
+ 
+         exact_ans = get_ans_exact(ans, exact, params)
+
+         return simplify(det(ans)), exact_ans
+     end
+     return ans
+ end
+
+fff = test(DX, y, [x1, x2, a, c, d])
+
+K = eval(build_function(fff, [x1, x2, a, c, d])[1])
+get_rank(K([1, 2, 3, 4, 5])) =#
+
+
+
+
+
+
+function is_NL_Observable(sys::Any, viewable::Any, params::Any, exact::Any = nothing)
+     n = length(viewable)
+     for i in (n+1):(length(sys)-1 + n)
+         push!(viewable, dot(Symbolics.gradient(viewable[i - n], params, simplify = true), sys))
+     end
 
     ans = Symbolics.jacobian(Y, params, simplify=true)
 
-    return simplify(det(ans))
+    if exact != nothing
 
+        exact_ans = get_ans_exact(ans, exact, params)
+
+        return simplify(det(ans)), exact_ans
+    end
+
+    return simplify(det(ans))
 end
 
-print(is_NL_Observable(DX, y, D, [x1, x2, a, c, d]))
+println(expand(is_NL_Observable(DX, y, [x1, x2, a, c, d])))
