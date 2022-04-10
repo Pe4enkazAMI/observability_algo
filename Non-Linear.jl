6ccb11a766ee58d8af70f3a1513c6c3d0c4c7cc6
Pe4enkazAMI <80426716+Pe4enkazAMI@users.noreply.github.com>
2022-04-10 18:22:27 +0300

    Updated

--- Non-Linear.jl
+++ Non-Linear.jl
@@ -22,7 +22,8 @@ function get_rank(Matr::Any, threshold = 1e-12)
      """
      U, S, VT = svd(Matr)
      S = filter(n -> n > threshold, S)
-     return length(S)
+     ans = count((i->(i > 0)),S)
+     return ans
 end 
 ########################
 
@@ -35,8 +36,8 @@ function get_ans_specific(Matr::Any, specific::Any, vars::Any)
           specific: specific vector interesting for us for some reason
           vars: variables of our system
      Output:
-          1 if interesting vector is observable.
-          0 if not.
+          True if interesting vector is observable.
+          False if not.
      """
 
     random_array = rand(Float32, length(vars))
@@ -49,55 +50,59 @@ function get_ans_specific(Matr::Any, specific::Any, vars::Any)
 
     rkMat = get_rank(_Only_Here_)
 
-    push!(_Only_Here_, specific)
+    hcat(_Only_Here_, specific)
 
     rkExMat = get_rank(_Only_Here_)
     if rkExMat == rkMat
-         return 1
+         return true
     else
-         return 0
+         return false
     end
 end
 ########################
 
 
 
-function is_NL_Observable(sys::Any, viewable::Any, params::Any, specific::Any = nothing)
-"""
-     Input:
-          sys: vector of functions that depicts some system. // STRICTLY in order: normal equation -> dummy parameter func.
-          viewable: actually it should be called "output", it is the vector which we observe during something.
-          params: vector of parameters obviously.
-          specific: not essential parameter, helps to find out the obs. information about some specific vector.
-     Output:
-          1 if system is observable.
-          0 if not.
-"""
-    rand_arr = rand(Float32, length(params))
-     n = length(viewable)
+function is_NL_Observable(sys::Any, output::Any, params::Any, specific::Any = nothing)
+     """
+          Input:
+               sys: vector of functions that depicts some system. // STRICTLY in order: normal equation -> dummy parameter func.
+               viewable: actually it should be called "output", it is the vector which we observe during something.
+               params: vector of parameters obviously.
+               specific: not essential parameter, helps to find out the obs. information about some specific vector.
+          Output:
+               True if system is observable.
+               False if not.
+     """
+     rand_arr = rand(Float32, length(params))
+     n = length(output)
      for i in (n+1):(length(sys)-1 + (n*length(params)))
-         push!(viewable, dot(Symbolics.gradient(viewable[i - n], params, simplify = true), sys))
+         push!(output, dot(Symbolics.gradient(output[i - n], params, simplify = true), sys))
      end
 
-    ans = Symbolics.jacobian(viewable, params, simplify=true)
-    shape = size(ans)
+     ans = Symbolics.jacobian(output, params, simplify=true)
+     shape = size(ans)
 
-    if specific != nothing
+     if specific != nothing
 
-        sp_ans = get_ans_specific(ans, specific, params)
+          sp_ans = get_ans_specific(ans, specific, params)
 
-        return simplify(det(ans)), sp_ans
-    end
+          return sp_ans
 
-    ans_rank = get_rank(Symbolics.value.(substitute(ans, Dict(params[i] => rand_arr[i] for i in 1:length(params)))))
+     end
 
-    if ans_rank == min(shape[1], shape[2])
-     return 1
-    else 
-     return 0
-    end
+     ans_rank = get_rank(Symbolics.value.(substitute(ans, Dict(params[i] => rand_arr[i] for i in 1:length(params)))))
 
+     if ans_rank == min(shape[1], shape[2])
+
+               return true
+
+     else 
+
+               return false
+
+     end
 end
 #smth
 
-println(expand(is_NL_Observable(DX, vec([y1, y2]), [x1, x2])))
+println(expand(is_NL_Observable(DX, vec([y1, y2]), [x1, x2], [x1, 0, 0, 0, 0])))
