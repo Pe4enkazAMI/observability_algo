8960c80ba843d457f2763dff2dfd1e44dfd41712
Pe4enkazAMI <80426716+Pe4enkazAMI@users.noreply.github.com>
2022-07-29 18:38:39 +0300

    Fixed

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,6 +6,18 @@ include("../Non-Linear.jl")
 
     @variables t x1(t) x2(t)
     xdot = [x2, exp(x2)]
-    @test is_NL_Observable(xdot, [x1, x2], vec([x1]))
+    y = x1
+    @test is_NL_Observable(xdot, [y], [x1, x2], nothing, true)
 
+    @variables t x(t) y(t) a b c d
+    xdot = [(a-b*y)*x, (-c+d*x)*y, a, b, c, d]
+    y = x + y
+    @test is_NL_Observable(DX, [y], [x], nothing, true)
+
+    @variables x1 x2
+    xdot = [x1^2 + 2*x1*x2, x2^2]
+    y = x1 + x2    
+    @test !is_NL_Observable(xdot, [y], [x1, x2], nothing, true)
+
+    
 end
